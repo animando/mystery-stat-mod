48bdc13a71be72fd85996382523b87203b97d521
Graham King <gking2224@gmail.com>
2024-04-24 09:37:47 +0100

    Remove redundant modmenu

--- src/main/java/uk/co/animandosolutions/mcdev/mysterystat/config/ConfigModel.java
+++ src/main/java/uk/co/animandosolutions/mcdev/mysterystat/config/ConfigModel.java
@@ -1,9 +1,7 @@
 package uk.co.animandosolutions.mcdev.mysterystat.config;
 
 import io.wispforest.owo.config.annotation.Config;
-import io.wispforest.owo.config.annotation.Modmenu;
 
-@Modmenu(modId = "mystery-stat")
 @Config(name = "mystery-stat", wrapperName = "MysteryStatConfig")
 public class ConfigModel {
     public long discordChannelId = -1L;
